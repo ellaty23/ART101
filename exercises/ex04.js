1ad8d2e896ddf3a61d51fa34144c50ddc7adcefe
ellaty23 <ellaty23@gmail.com>
2026-04-24 14:49:46 -0700

    April24

--- exercises/ex04.js
+++ exercises/ex04.js
@@ -1,9 +1,26 @@
 let count = 0;
 
+let colors = ["Orchid", "Coral", "HotPink", "Plum"];
+
+let colorCount;
+
+let pencil = {
+    color: "Yellow",
+    isSharp: false,
+};
+
 $("#needy-button").click(function () {
 
-    count = count + 3;
+    count = count + 1;
 
-    $("#needy-button").html("I have slept for " + count + " minutes")
+    colorCount = count - 1
+
+    $("#needy-button").html("I have slept for " + count + " minutes dreaming of " + colors[colorCount]);
+
+    $("#needy-button").css("background-color", colors[colorCount]);
+
+    $("html").css("background-color", colors[colorCount + 1]);
+
+    console.log(pencil.color);
 
 });
